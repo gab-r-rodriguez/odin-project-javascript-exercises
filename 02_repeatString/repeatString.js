bb6a8b9a7098d0484523a4db5ad2a82c9bd78c00
Gabriel Rodriguez <gabrielroquerodriguez@gmail.com>
2022-12-27 15:19:15 -0800

    Answer exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (stringToInput, timesToReturn) {
+    let repeatedString = '';
 
+    if (timesToReturn > 0) {
+        while (timesToReturn > 0) {
+            repeatedString += stringToInput;
+            timesToReturn--; // decrement timesToReturn to close loop
+        }
+        return repeatedString;
+    }
+    else if (timesToReturn == 0) {
+        return repeatedString;
+    }
+    else return 'ERROR';
 };
 
 // Do not edit below this line
